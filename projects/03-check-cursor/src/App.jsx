549d7fa7c37206b3b17ba42088aa3fbbadfeb57c
Pablo Garcia <garciandres.15@gmail.com>
2023-08-25 05:25:54 +0200

    03 - Check cursor

---
+++ projects/03-check-cursor/src/App.jsx
@@ -0,0 +1,11 @@
+import { CheckCursor } from "./components/CheckCursor";
+
+function App() {
+  return (
+    <main>
+      <CheckCursor />
+    </main>
+  );
+}
+
+export default App;
---
+++ projects/03-check-cursor/src/components/CheckCursor.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+export function CheckCursor() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const buttonText = `${enabled ? "Desactivar" : "Activar"} seguir puntero`;
+
+  useEffect(() => {
+    console.log("Effect ", { enabled });
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log("Handle Move ", { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    };
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+
+    return () => {
+      //RETURN FUNCTION TO CLEAN THE EFFECT
+      //THIS RUNS WHEN
+      //  - THE COMPONENT UNMOUNTS
+      //  - THE DEPENDENCIES CHANGE
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>{buttonText}</button>
+    </>
+  );
+}
