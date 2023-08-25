a58c2f944a1ee49f76751a7375e15e356e59873f
Pablo Garcia <garciandres.15@gmail.com>
2023-08-25 05:38:58 +0200

    Toggle cursor

--- projects/03-check-cursor/src/components/CheckCursor.jsx
+++ projects/03-check-cursor/src/components/CheckCursor.jsx
@@ -27,6 +27,14 @@ export function CheckCursor() {
     };
   }, [enabled]);
 
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled);
+
+    return () => {
+      document.body.classList.remove("no-cursor");
+    };
+  }, [enabled]);
+
   return (
     <>
       <div
